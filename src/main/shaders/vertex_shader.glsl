4fea835d06ab383d0178e6ffb83d62967a9ef782
Viet Nguyen <68336722+viet2007ht@users.noreply.github.com>
2025-12-30 00:45:13 +0700

    Delete src/main/shaders directory

--- src/main/shaders/vertex_shader.glsl
+++
@@ -1,9 +0,0 @@
-attribute vec4 a_Position;
-attribute vec2 a_TexCoord;
-uniform mat4 u_Transform;
-varying vec2 v_TexCoord;
-
-void main() {
-    gl_Position = u_Transform * a_Position;
-    v_TexCoord = a_TexCoord;
-}
